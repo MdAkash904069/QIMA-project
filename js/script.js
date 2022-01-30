5780d28bcd0608f5a0e34789c64d1fba23c83ea0
Md Akash <86820955+MdAkash904069@users.noreply.github.com>
2022-01-30 11:50:43 +0600

    Initial commit

---
+++ js/script.js
@@ -0,0 +1,84 @@
+$(document).ready(function(){
+
+  $(window).on('scroll', function() {
+        var scroll = $(window).scrollTop();
+        if (scroll >= 100) {
+            $(".logo").css('height','30');
+            $(".nav-create").css('display','block');
+            $(".nav-right2").css('display','block');
+            $(".nav-right1").css('display','none');
+
+        } else {
+            $(".logo").css('height','60');
+            $(".nav-create").css('display','none');
+            $(".nav-right2").css('display','none');
+            $(".nav-right1").css('display','block');
+        }
+    });
+
+    
+
+
+  $("#our_solution").hover(function(){
+    $(".our-solution").css('display','block');
+    $("#our_solution").addClass('navbar-li');
+  });
+  $(".our-solution").hover(function(){
+    $(".our-solution").css('display','block');
+    $("#our_solution").addClass('navbar-li');
+  });
+  $(".our-solution").mouseleave(function(){
+    $(".our-solution").css('display','none');
+    $("#our_solution").removeClass('navbar-li');
+  });
+  $('#our_solution').mouseleave(function () {
+    $(".our-solution").css('display','none');
+    $("#our_solution").removeClass('navbar-li');
+       
+    });
+
+
+  $("#industry").hover(function(){
+    $(".industry").css('display','block');
+    $("#industry").addClass('navbar-li');
+  });
+  $(".industry").hover(function(){
+    $(".industry").css('display','block');
+    $("#industry").addClass('navbar-li');
+  });
+  $('.industry').mouseleave(function () {
+    $(".industry").css('display','none'); 
+    $("#industry").removeClass('navbar-li');
+    });
+  $('#industry').mouseleave(function () {
+    $(".industry").css('display','none'); 
+    $("#industry").removeClass('navbar-li');
+    });
+
+   $("#about-us").hover(function(){
+    $(".about-us").css('display','block');
+    $("#about-us").addClass('navbar-li');
+  });
+    $(".about-us").hover(function(){
+    $(".about-us").css('display','block');
+    $("#about-us").addClass('navbar-li');
+  });
+
+  $('#about-us').mouseleave(function () {
+    $(".about-us").css('display','none');
+    $("#about-us").removeClass('navbar-li');
+       
+    });
+    $(".about-us").mouseleave(function(){
+    $(".about-us").css('display','none');
+    $("#about-us").removeClass('navbar-li');
+  });
+
+
+
+
+
+
+
+     
+});
